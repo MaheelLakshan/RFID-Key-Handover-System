98c9fe5d380600ca954d42700222e391655093fa
MaheelLakshan <115983012+MaheelLakshan@users.noreply.github.com>
2023-03-20 15:18:46 +0530

    Update firebase.js

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -6,12 +6,7 @@
 
 // // // Your web app's Firebase configuration
 // // const firebaseConfig = {
-// //   apiKey: 'AIzaSyDx3sFoHn7IaEzg4B2v0IJxYXbpl_6XRKg',
-// //   authDomain: 'rfidkeyhandoversystem.firebaseapp.com',
-// //   projectId: 'rfidkeyhandoversystem',
-// //   storageBucket: 'rfidkeyhandoversystem.appspot.com',
-// //   messagingSenderId: '589260498775',
-// //   appId: '1:589260498775:web:f1246410789c0b1475c012',
+// //  
 // // };
 
 // // // Initialize Firebase
@@ -27,14 +22,7 @@
 // // Your web app's Firebase configuration
 
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyDx3sFoHn7IaEzg4B2v0IJxYXbpl_6XRKg',
-//   authDomain: 'rfidkeyhandoversystem.firebaseapp.com',
-//   databaseURL:
-//     'https://rfidkeyhandoversystem-default-rtdb.asia-southeast1.firebasedatabase.app',
-//   projectId: 'rfidkeyhandoversystem',
-//   storageBucket: 'rfidkeyhandoversystem.appspot.com',
-//   messagingSenderId: '589260498775',
-//   appId: '1:589260498775:web:0cc01a44090f9fc675c012',
+//   
 // };
 
 // // Initialize Firebase
